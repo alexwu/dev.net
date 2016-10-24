fae3115d4363e4d03fa1156737cf57ef2b34bba7
Alex Wu <alexwu96@gmail.com>
2016-10-23 17:01:57 -0700

    It now goes to a post page when card object is clicked

--- app/src/main/java/cse110group4/devnet/RVAdapter.java
+++ app/src/main/java/cse110group4/devnet/RVAdapter.java
@@ -1,5 +1,6 @@
 package cse110group4.devnet;
 
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -25,6 +26,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
             personName = (TextView)itemView.findViewById(R.id.person_name);
             personAge = (TextView)itemView.findViewById(R.id.person_age);
             personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+
         }
     }
 
@@ -51,6 +53,22 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
         personViewHolder.personName.setText(projects.get(i).name);
         personViewHolder.personAge.setText(projects.get(i).age);
         personViewHolder.personPhoto.setImageResource(projects.get(i).photoId);
+
+        personViewHolder.cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //implement onClick
+
+                Intent post = new Intent(v.getContext(), PostPage.class);
+                post.putExtra("title", "Wow what a cool title");
+                post.putExtra("content", "Wow what a cool post");
+
+                System.out.println("Clicked");
+                v.getContext().startActivity(post);
+
+            }
+        });
+
     }
 
     @Override
