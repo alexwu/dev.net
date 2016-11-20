a5fa1bd3469621a5a2264a3e033a09e93eb9a801
Alex Wu <alexwu96@gmail.com>
2016-11-20 03:57:31 -0800

    Invisible favorites button added to the top right corner of the post page, just need to connect it to firebase and make it visible

--- app/src/main/java/cse110group4/devnet/PostPage.java
+++ app/src/main/java/cse110group4/devnet/PostPage.java
@@ -31,7 +31,6 @@ public class PostPage extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,4 +54,25 @@ public class PostPage extends AppCompatActivity {
         post_content.setText(intent.getStringExtra("content"));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.post_page_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
